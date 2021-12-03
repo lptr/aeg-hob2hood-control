8c810407f8b2437ed81516f2ab814e7ca6a49de5
Lóránt Pintér <lorant.pinter@gmail.com>
2021-12-03 08:56:49 +0100

    Polish code

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,19 @@
 
 const uint8_t irPin = D5;
 
-const uint8_t dataInPin = D7;
-const uint8_t dataOutPin = D8;
+// IR commands from AEG hob2hood device
+const uint64_t IRCMD_VENT_1 = 0xE3C01BE2;       // Hob2hood On (level 1)
+const uint64_t IRCMD_VENT_2 = 0xD051C301;       // Hob2hood level 2
+const uint64_t IRCMD_VENT_3 = 0xC22FFFD7;       // Hob2hood level 3
+const uint64_t IRCMD_VENT_4 = 0xB9121B29;       // Hob2hood level 4
+const uint64_t IRCMD_VENT_OFF = 0x55303A3;      // Hob2hood off
+const uint64_t IRCMD_LIGHT_ON = 0xE208293C;     // Light on (Hood on)
+const uint64_t IRCMD_LIGHT_OFF = 0x24ACF947;    // Light off (Automatic after 2min)
 
 IRrecv irReceiver(irPin);
 
-decode_results results;
+const uint8_t dataInPin = D7;
+const uint8_t dataOutPin = D8;
 
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
@@ -27,15 +34,6 @@ void setup() {
     Serial.print("IRrecvDemo is now running and waiting for IR message on Pin ");
     Serial.println(irPin);
 }
-
-// IR commands from AEG hob2hood device
-const uint64_t IRCMD_VENT_1 = 0xE3C01BE2;       // Hob2hood On (level 1)
-const uint64_t IRCMD_VENT_2 = 0xD051C301;       // Hob2hood level 2
-const uint64_t IRCMD_VENT_3 = 0xC22FFFD7;       // Hob2hood level 3
-const uint64_t IRCMD_VENT_4 = 0xB9121B29;       // Hob2hood level 4
-const uint64_t IRCMD_VENT_OFF = 0x55303A3;      // Hob2hood off
-const uint64_t IRCMD_LIGHT_ON = 0xE208293C;     // Light on (Hood on)
-const uint64_t IRCMD_LIGHT_OFF = 0x24ACF947;    // Light off (Automatic after 2min)
 
 bool handleIrCode(const decode_results& results) {
     Serial.print("Received: ");
@@ -92,6 +90,7 @@ void loop() {
         Serial.println();
     }
 
+    decode_results results;
     if (irReceiver.decode(&results)) {
         digitalWrite(LED_BUILTIN, LOW);
         if (handleIrCode(results)) {
