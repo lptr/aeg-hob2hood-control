5fcfebec54ec4f364f8b6f7b9808a3fe84c713e9
Lóránt Pintér <lorant.pinter@gmail.com>
2021-12-03 09:25:30 +0100

    Capture data input

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
+#include <IRac.h>
 #include <IRrecv.h>
+#include <IRtext.h>
 #include <IRutils.h>
 
 const uint8_t irPin = D5;
@@ -18,21 +20,95 @@ IRrecv irReceiver(irPin);
 const uint8_t dataInPin = D7;
 const uint8_t dataOutPin = D8;
 
+const uint16_t kCaptureBufferSize = 1024;
+
+// kTimeout is the Nr. of milli-Seconds of no-more-data before we consider a
+// message ended.
+// This parameter is an interesting trade-off. The longer the timeout, the more
+// complex a message it can capture. e.g. Some device protocols will send
+// multiple message packets in quick succession, like Air Conditioner remotes.
+// Air Coniditioner protocols often have a considerable gap (20-40+ms) between
+// packets.
+// The downside of a large timeout value is a lot of less complex protocols
+// send multiple messages when the remote's button is held down. The gap between
+// them is often also around 20+ms. This can result in the raw data be 2-3+
+// times larger than needed as it has captured 2-3+ messages in a single
+// capture. Setting a low timeout value can resolve this.
+// So, choosing the best kTimeout value for your use particular case is
+// quite nuanced. Good luck and happy hunting.
+// NOTE: Don't exceed kMaxTimeoutMs. Typically 130ms.
+#if DECODE_AC
+// Some A/C units have gaps in their protocols of ~40ms. e.g. Kelvinator
+// A value this large may swallow repeats of some protocols
+const uint8_t kTimeout = 50;
+#else     // DECODE_AC
+// Suits most messages, while not swallowing many repeats.
+const uint8_t kTimeout = 15;
+#endif    // DECODE_AC
+// Alternatives:
+// const uint8_t kTimeout = 90;
+// Suits messages with big gaps like XMP-1 & some aircon units, but can
+// accidentally swallow repeated messages in the rawData[] output.
+//
+// const uint8_t kTimeout = kMaxTimeoutMs;
+// This will set it to our currently allowed maximum.
+// Values this high are problematic because it is roughly the typical boundary
+// where most messages repeat.
+// e.g. It will stop decoding a message and start sending it to serial at
+//      precisely the time when the next message is likely to be transmitted,
+//      and may miss it.
+
+// Set the smallest sized "UNKNOWN" message packets we actually care about.
+// This value helps reduce the false-positive detection rate of IR background
+// noise as real messages. The chances of background IR noise getting detected
+// as a message increases with the length of the kTimeout value. (See above)
+// The downside of setting this message too large is you can miss some valid
+// short messages for protocols that this library doesn't yet decode.
+//
+// Set higher if you get lots of random short UNKNOWN messages when nothing
+// should be sending a message.
+// Set lower if you are sure your setup is working, but it doesn't see messages
+// from your device. (e.g. Other IR remotes work.)
+// NOTE: Set this value very high to effectively turn off UNKNOWN detection.
+const uint16_t kMinUnknownSize = 1024;
+
+// How much percentage lee way do we give to incoming signals in order to match
+// it?
+// e.g. +/- 25% (default) to an expected value of 500 would mean matching a
+//      value between 375 & 625 inclusive.
+// Note: Default is 25(%). Going to a value >= 50(%) will cause some protocols
+//       to no longer match correctly. In normal situations you probably do not
+//       need to adjust this value. Typically that's when the library detects
+//       your remote's message some of the time, but not all of the time.
+const uint8_t kTolerancePercentage = kTolerance;    // kTolerance is normally 25%
+
+// Use turn on the save buffer feature for more complete capture coverage.
+IRrecv dataReceiver(dataInPin, kCaptureBufferSize, kTimeout, true);
+
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
-    pinMode(dataInPin, INPUT);
     pinMode(dataOutPin, OUTPUT);
+    digitalWrite(dataOutPin, HIGH);
 
     Serial.begin(115200);
     while (!Serial) {
         delay(50);
     }
+    // Perform a low level sanity checks that the compiler performs bit field
+    // packing as we expect and Endianness is as we expect.
+    assert(irutils::lowLevelSanityCheck() == 0);
 
     irReceiver.enableIRIn();
 
+#if DECODE_HASH
+    // Ignore messages with less than minimum on or off pulses.
+    dataReceiver.setUnknownThreshold(kMinUnknownSize);
+#endif                                                  // DECODE_HASH
+    dataReceiver.setTolerance(kTolerancePercentage);    // Override the default tolerance.
+    dataReceiver.enableIRIn();                          // Start the receiver
+
     Serial.println();
-    Serial.print("IRrecvDemo is now running and waiting for IR message on Pin ");
-    Serial.println(irPin);
+    Serial.printf("IR on pin %d, data in on pin %d\n", irPin, dataInPin);
 }
 
 bool handleIrCode(const decode_results& results) {
@@ -79,17 +155,7 @@ bool handleIrCode(const decode_results& results) {
     }
 };
 
-int previousData = -1;
-
 void loop() {
-    int data = digitalRead(dataInPin);
-    if (data != previousData) {
-        previousData = data;
-        Serial.print("Data: ");
-        serialPrintUint64(data, HEX);
-        Serial.println();
-    }
-
     decode_results results;
     if (irReceiver.decode(&results)) {
         digitalWrite(LED_BUILTIN, LOW);
@@ -98,6 +164,39 @@ void loop() {
         }
         irReceiver.resume();
     }
-    delay(100);
-    digitalWrite(LED_BUILTIN, HIGH);
+
+    // Check if the code has been received.
+    if (dataReceiver.decode(&results)) {
+        // Display a crude timestamp.
+        uint32_t now = millis();
+        Serial.printf(D_STR_TIMESTAMP " : %06u.%03u\n", now / 1000, now % 1000);
+
+        // Check if we got an IR message that was to big for our capture buffer.
+        if (results.overflow) {
+            Serial.printf(D_WARN_BUFFERFULL "\n", kCaptureBufferSize);
+        }
+
+        // Display the library version the message was captured with.
+        Serial.println(D_STR_LIBRARY "   : v" _IRREMOTEESP8266_VERSION_ "\n");
+        // Display the tolerance percentage if it has been change from the default.
+        if (kTolerancePercentage != kTolerance)
+            Serial.printf(D_STR_TOLERANCE " : %d%%\n", kTolerancePercentage);
+        // Display the basic output of what we found.
+        Serial.print(resultToHumanReadableBasic(&results));
+
+        // Display any extra A/C info if we have it.
+        String description = IRAcUtils::resultAcToString(&results);
+        if (description.length()) {
+            Serial.println(D_STR_MESGDESC ": " + description);
+        }
+        yield();    // Feed the WDT as the text output can take a while to print.
+
+        // Output the results as source code
+        Serial.println(resultToSourceCode(&results));
+        Serial.println();    // Blank line between entries
+        yield();             // Feed the WDT (again)
+    }
+
+    // delay(100);
+    // digitalWrite(LED_BUILTIN, HIGH);
 }
